973b19b5e686eb9c248d9ce0dc364591c9c3b0cb
Hramchenlo Alexey <qwefix@gmail.com>
2020-09-28 18:17:54 +0300

    feat: task ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array||array.length==0){return 0};
+  let minItem=array[1];
+  for(let item of array){
+    if (item<minItem){
+      minItem=item
+    }
+  }
+  return minItem
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array||array.length==0){return 0};
+  let maxItem=array[1];
+  for(let item of array){
+    if (item>maxItem){
+      maxItem=item
+    }
+  }
+  return maxItem
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array||array.length==0){return 0};
+  let sum=0;
+  let length=array.length;
+  for(let item of array){
+    sum+=item;
+  }
+  return sum/length;
+
 }
